de5afefa0c3cf9992f4c8d752e70b0d5b403be6b
alekya pochampally <S536776@nwmissouri.edu>
2019-09-22 15:20:55 -0500

    tax calculator app

---
+++ practise.js
@@ -0,0 +1,14 @@
+function Calculation()
+ { 
+     const Total=parseFloat(document.querySelector('#field_tot').value)
+     const Rate=parseFloat(document.querySelector('#field_rate').value)
+     console.log(Rate);
+     const Tip=Total*(Rate/100)
+     const TaxRate=5.5
+     const Tax=Total*(TaxRate/100)
+     const GrandTotal=Total+Tip+Tax;
+     document.querySelector('#Tip').innerHTML=`$${Tip.toFixed(2)}`
+     document.querySelector('#Tax').innerHTML=`$${Tax.toFixed(2)}`
+     document.querySelector('#Total').innerHTML=`$${GrandTotal.toFixed(2)}`
+ }
+     document.querySelector('#button').addEventListener('click',Calculation)
